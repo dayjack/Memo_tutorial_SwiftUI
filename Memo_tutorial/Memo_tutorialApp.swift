4a3e9fa9317495c3b4b4539383d68871a810ca92
dayjack <frostjanna12@gmail.com>
2023-03-16 16:46:27 +0900

    Initial Commit

---
+++ Memo_tutorial/Memo_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memo_tutorialApp.swift
+//  Memo_tutorial
+//
+//  Created by ChoiYujin on 2023/03/16.
+//
+
+import SwiftUI
+
+@main
+struct Memo_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
